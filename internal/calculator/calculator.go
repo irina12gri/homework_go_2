a17ec9b742b57aa73bb8acaad069b0bbbc824e88
irina12gri <irka12serenko@gmail.com>
2026-09-09 09:26:31 +0300

    Done calculator

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "errors"
+
 // Блок calculator — объединяющая задача.
 // Здесь нужно закрепить:
 // int, string operation, switch, if, error,
@@ -9,5 +11,27 @@ package calculator
 //
 // TODO: поддержите операции "+", "-", "*", "/" и "%" над a и b. Деление выполняется как целочисленное; нулевой делитель для "/" и "%", пустая или неизвестная операция должны возвращать ошибку.
 func Calculate(a, b int, operation string) (int, error) {
-	return 0, nil
+	switch operation {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		{
+			if b == 0 {
+				return 0, errors.New("b is zero")
+			}
+			return a / b, nil
+		}
+	case "%":
+		{
+			if b == 0 {
+				return 0, errors.New("b is zero")
+			}
+			return a % b, nil
+		}
+	}
+	return 0, errors.New("unknown operation")
 }
